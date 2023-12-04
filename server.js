1c3bb3af11c583f88b9c73c7d007d6f4789d8fc5
marli556 <133556194+marli556@users.noreply.github.com>
2023-12-03 18:00:56 -0800

    Update server.js
    I added the model for creating the book and also the GET and POST for it

--- server.js
+++ server.js
@@ -12,6 +12,36 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api', routes);
 
+// Creating mongoose model for books
+const Book = mongoose.model('Book', {
+  title: String,
+  author: String,
+  genre: String,
+  releaseYear: Number
+});
+
+// API to add a new book
+app.post('/books', async (req, res) => {
+  try {
+    const { title, author, genre, releaseYear } = req.body;
+    const newBook = new Book({ title, author, genre });
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// API to get all books
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/bookstore', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
